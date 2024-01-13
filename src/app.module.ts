89a017cbfb042ce2194270b342459a41430f8865
Moahened947 <mohanedtoha@gmail.com>
2024-01-13 03:20:10 +0200

    First

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { RisksModule } from './risks/risks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Risk } from './risks/entities/risk.entity';
+import { ConfigModule } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 
 @Module({
-  imports: [RisksModule,TypeOrmModule.forRoot({
+  imports: [RisksModule,ConfigModule.forRoot(),TypeOrmModule.forRoot({
     type: 'mysql',
     host: 'localhost',
     port: 3306,
